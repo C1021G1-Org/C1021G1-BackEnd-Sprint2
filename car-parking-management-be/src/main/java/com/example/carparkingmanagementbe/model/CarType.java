67a9c4b7369856a39633d41e5ee2683e95af6008
ductinh0402 <hoangductinh0402@gmail.com>
2022-05-25 12:33:35 +0700

    tinh puss update cartype

---
+++ car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/model/CarType.java
@@ -0,0 +1,28 @@
+package com.example.carparkingmanagementbe.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "car_type")
+public class CarType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+
+    @JsonBackReference
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "carType")
+    private Set<Car> carSet;
+}
