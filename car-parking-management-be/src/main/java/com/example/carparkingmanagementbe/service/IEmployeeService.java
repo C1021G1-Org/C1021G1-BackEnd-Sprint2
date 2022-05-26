8c00faf4ce6ee45be7ed7aa0ea2b33c959d7be2e
WiLynd <hdqphu.dev@gmail.com>
2022-05-26 14:17:22 +0700

    push-BE-PhuHDQ

--- car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/service/IEmployeeService.java
+++ car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/service/IEmployeeService.java
@@ -1,4 +1,19 @@
 package com.example.carparkingmanagementbe.service;
 
+import com.example.carparkingmanagementbe.dto.EmployeeDto;
+import com.example.carparkingmanagementbe.model.Employee;
+
+import java.util.Optional;
+
 public interface IEmployeeService {
+    void createEmployee(EmployeeDto employeeDto);
+
+    Optional<Employee> findEmployeeById(Long id);
+
+    void updateEmployee(EmployeeDto employeeDto);
+
+    Integer findByEmailNot(Long id, String email);
+
+    Integer findByPhoneNot(Long id, String phone);
+
 }
