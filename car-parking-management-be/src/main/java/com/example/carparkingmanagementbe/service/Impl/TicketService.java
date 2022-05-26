4f81174b889ae14464894c0c5e0141b51fbe5965
thanhlong186 <thanhlong180696@gmail.com>
2022-05-26 17:40:12 +0700

    push

--- car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/service/Impl/TicketService.java
+++ car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/service/Impl/TicketService.java
@@ -1,8 +1,25 @@
 package com.example.carparkingmanagementbe.service.Impl;
 
+import com.example.carparkingmanagementbe.model.Ticket;
+import com.example.carparkingmanagementbe.repository.TicketRepository;
 import com.example.carparkingmanagementbe.service.ITicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TicketService implements ITicketService {
+    //    LongLT
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Override
+    public Ticket findTicketById(Long id) {
+
+        return ticketRepository.findByIdTicket(id);
+    }
+    @Override
+    public void updateTicket(Long idLocation, Double sumPrice, Long idTicketType, String endDate, Long idTicket) {
+        ticketRepository.updateTicket(idLocation, sumPrice, idTicketType, endDate, idTicket);
+    }
+//    LongLT
 }
