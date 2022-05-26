0ade5c9fc05a27d6f515b49d9c31199cc387db5c
LongTien <h2so4allii@gmail.com>
2022-05-26 09:33:08 +0700

    longtien

--- car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/controller/NewsController.java
+++ car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/controller/NewsController.java
@@ -1,7 +1,45 @@
 package com.example.carparkingmanagementbe.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.carparkingmanagementbe.model.News;
+import com.example.carparkingmanagementbe.service.Impl.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+//@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api/news")
 public class NewsController {
+    @Autowired
+    private NewsService service;
+
+    @PostMapping("/createNews")
+    public ResponseEntity<?> createNews(@RequestBody News news){
+        service.createNews(news);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/findNews/{id}")
+    public ResponseEntity<News> findNewsById(@PathVariable Long id){
+        if(id == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            News news = service.findNewsById(id);
+            return new ResponseEntity<News>(news, HttpStatus.OK);
+        }
+    }
+
+    @PatchMapping("/updateNews/{id}")
+    public ResponseEntity<?> updateNews(@PathVariable Long id, @RequestBody News news) {
+        if (news == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            service.updateNews(id, news);
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        }
+    }
+
+
+
 }
--- car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/service/INewsService.java
+++ car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/service/INewsService.java
@@ -1,4 +1,11 @@
 package com.example.carparkingmanagementbe.service;
 
+import com.example.carparkingmanagementbe.model.News;
+
+import java.util.Optional;
+
 public interface INewsService {
+    News findNewsById(Long id);
+    void createNews(News news);
+    void updateNews(Long id, News news);
 }
