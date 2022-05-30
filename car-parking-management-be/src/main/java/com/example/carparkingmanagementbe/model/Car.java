77cd0fcd0671a6ed48791b324dec406df0e22a86
trong19955 <trananhtrong19955@gmail.com>
2022-05-30 18:07:30 +0700

    pull trongta

--- car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/model/Car.java
+++ car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/model/Car.java
@@ -24,10 +24,10 @@ public class Car {
     private String carPlate;
     private String carCompany;
 
-    @Column(columnDefinition = "DATE")
-    private String startDate;
-    @Column(columnDefinition = "DATE")
-    private String endDate;
+//    @Column(columnDefinition = "DATE")
+//    private String startDate;
+//    @Column(columnDefinition = "DATE")
+//    private String endDate;
 
     private Boolean delFlag;
     @ManyToOne
--- car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/repository/LocationRepository.java
+++ car-parking-management-be/src/main/java/com/example/carparkingmanagementbe/repository/LocationRepository.java
@@ -14,14 +14,9 @@ import com.example.carparkingmanagementbe.model.AllowedCarParking;
 import com.example.carparkingmanagementbe.model.Location;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import org.springframework.data.web.PageableDefault;
 
 
-import com.example.carparkingmanagementbe.dto.LocationList;
+
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,12 +24,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import java.util.Optional;
-
-//location.description,\n" +
-//        "   location.width, location.is_empty, location.number,\n" +
-//        "   location.height, location.length, location.del_flag,\n" +
-//        "   location.id_floor,
 
 import java.util.Set;
 
